297a8a526a52b56ae716b4b1d0dcf90941863f5f
marcelopython <msr88957@gmail.com>
2021-01-08 22:20:37 -0300

    Evento para atualizar o todoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,11 +3,12 @@ import Grid from '../template/gride'
 import IconButton from '../template/IconButton'
 
 export default props => {
-    console.log(props)
     return (
     <div role="form" className="todoForm form-row">
         <Grid cols="12 9 10">
-            <input id="description" className="form-control" placeholder="Adicione uma tarefa" />
+            <input id="description" className="form-control" placeholder="Adicione uma tarefa" 
+            onChange={props.handleChange}
+            value={props.description}/>
         </Grid>
         <Grid cols="12 3 2">
             <IconButton icon="plus" style="primary" onClick={props.handleAdd}/>
